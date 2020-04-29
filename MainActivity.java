9d32ce4214b2f42aebc46a8bafbcd0a77dd3bbfd
Wen-Cong <64005211+Wen-Cong@users.noreply.github.com>
2020-04-29 17:05:30 +0800

    Added 3 buttons and a textview. add function to select new mole after each click and also added point scoring system.

--- MainActivity.java
+++ MainActivity.java
@@ -1,38 +1,119 @@
-package sg.edu.np.WhackAMole;
+package com.example.week1_practical;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
-
-
+import android.view.View;
+import android.view.ViewDebug;
+import android.widget.Button;
+import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    /* Hint
-        - The function setNewMole() uses the Random class to generate a random value ranged from 0 to 2.
-        - Feel free to modify the function to suit your program.
-    */
+    private Button ButtonLeft;
+    private Button ButtonMiddle;
+    private Button ButtonRight;
+    private TextView scores;
+    private int point;
+    private static final String TAG = "ButtonActivity";
+    private static final String TAG1 =  "Whack-A-Mole";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButtonLeft = (Button) findViewById(R.id.ButtonLeft);
+        ButtonMiddle = (Button) findViewById(R.id.ButtonMiddle);
+        ButtonRight = (Button) findViewById(R.id.ButtonRight);
+        scores = (TextView) findViewById(R.id.Score);
+        point = 0;
         Log.v(TAG, "Finished Pre-Initialisation!");
+
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-        setNewMole();
         Log.v(TAG, "Starting GUI!");
+        setNewMole();
+
+        ButtonLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Button Left pressed!");
+                if(ButtonLeft.getText() == "*"){
+                    point += 1;
+                    Log.v(TAG1, "Scores a Point\n" + point);
+                    scores.setText("" + point);
+                }
+                else if (ButtonLeft.getText() != "*"){
+                    point -= 1;
+                    Log.v(TAG1, "Missed a Whack!\n" + point);
+                    scores.setText("" + point);
+                }
+                setNewMole();
+
+            }
+        });
+        ButtonMiddle.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Log.v(TAG, "Button Middle Pressed!");
+                if(ButtonMiddle.getText() == "*"){
+                    point += 1;
+                    Log.v(TAG1, "Scores a Point\n" + point);
+                    scores.setText("" + point);
+                }
+                else if (ButtonMiddle.getText() != "*"){
+                    point -= 1;
+                    Log.v(TAG1, "Missed a Whack!\n" + point);
+                    scores.setText("" + point);
+                }
+                setNewMole();
+            }
+        });
+        ButtonRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Button Right Pressed!");
+                if(ButtonRight.getText() == "*"){
+                    point += 1;
+                    Log.v(TAG1, "Scores a Point\n" + point);
+                    scores.setText("" + point);
+                }
+                else if (ButtonRight.getText() != "*"){
+                    point -= 1;
+                    Log.v(TAG1, "Missed a Whack!\n" + point);
+                    scores.setText("" + point);
+                }
+                setNewMole();
+            }
+        });
     }
+
 
 
     public void setNewMole()
     {
         Random ran = new Random();
         int randomLocation = ran.nextInt(3);
+        if(randomLocation == 0){
+            ButtonLeft.setText("*");
+        }
+        else if (randomLocation != 0){
+            ButtonLeft.setText("o");
+        }
+        if(randomLocation == 1){
+            ButtonMiddle.setText("*");
+        }
+        else if (randomLocation != 1){
+            ButtonMiddle.setText("o");
+        }
+        if(randomLocation == 2){
+            ButtonRight.setText("*");
+        }
+        else if (randomLocation != 2){
+            ButtonRight.setText("o");
+        }
     }
 }
